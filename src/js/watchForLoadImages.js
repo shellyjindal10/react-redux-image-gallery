1b7a8ed402cf0c9ffac05e81b4d99e81d85b610b
Shelly Jindal <sjindal@C02PHGACG3QC.local>
2016-12-19 09:44:59 -0800

    indentation

--- src/js/watchForLoadImages.js
+++ src/js/watchForLoadImages.js
@@ -2,13 +2,13 @@ import {fetchImages} from './server/flickr';
 import {put, take, fork} from 'redux-saga/effects';
  
 export function* loadSomeImages() {
-	 try {
-	   const images = yield fetchImages()
-	   yield put({type: 'IMAGES_LOADED', images})
+  try {
+    const images = yield fetchImages()
+    yield put({type: 'IMAGES_LOADED', images})
     console.log(images)
-	 } catch(error) {
+  } catch(error) {
     yield put({type: 'IMAGES_LOADED', error})
-	 }
+  }
 }
 
 export function* watchForLoadImages() {
